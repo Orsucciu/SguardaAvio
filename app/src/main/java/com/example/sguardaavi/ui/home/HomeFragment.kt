22b189647e46bf6f154fa338af1007d44cfd1192
20161001 <theo181297@gmail.com>
2019-11-19 16:50:04 +0100

    time picker and stuff

--- app/src/main/java/com/example/sguardaavi/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/sguardaavi/ui/home/HomeFragment.kt
@@ -1,27 +1,32 @@
 package com.example.sguardaavi.ui.home
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.sguardaavi.FlightApplication
 import com.example.sguardaavi.R
-import java.io.IOException
 import com.example.sguardaavi.Utils
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home.view.*
+import kotlinx.android.synthetic.main.nav_header_main.view.*
+import java.util.*
+import kotlin.collections.ArrayList
+
+//https://android--code.blogspot.com/2018/02/android-kotlin-autocompletetextview.html tuto for the autocomplete thing
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+
+    lateinit var fromDate: EditText
+    lateinit var toDate: EditText
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,6 +37,14 @@ class HomeFragment : Fragment() {
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
+        val switchD: Switch = root.findViewById(R.id.switch1)
+
+        fromDate = root.findViewById(R.id.dateD)
+        toDate = root.findViewById(R.id.dateA)
+
+        val currentCalendar = Calendar.getInstance()
+        updateDateLabel(fromDate, currentCalendar)
+        updateDateLabel(toDate, currentCalendar)
 
         homeViewModel.text.observe(this, Observer { myString ->
             textView.text = myString
@@ -53,16 +66,16 @@ class HomeFragment : Fragment() {
 
 
         // Set the AutoCompleteTextView adapter
-        root.autocompleteTextView.setAdapter(adapter)
+        root.text_home.setAdapter(adapter)
 
 
         // Auto complete threshold
         // The minimum number of characters to type to show the drop down
-        root.autocompleteTextView.threshold = 1
+        root.text_home.threshold = 1
 
 
         // Set an item click listener for auto complete text view
-        root.autocompleteTextView.onItemClickListener = AdapterView.OnItemClickListener{
+        root.text_home.onItemClickListener = AdapterView.OnItemClickListener{
                 parent,view,position,id->
             val selectedItem = parent.getItemAtPosition(position).toString()
             // Display the clicked item using toast
@@ -71,27 +84,109 @@ class HomeFragment : Fragment() {
 
 
         // Set a dismiss listener for auto complete text view
-        root.autocompleteTextView.setOnDismissListener {
+        root.text_home.setOnDismissListener {
             Toast.makeText(FlightApplication.appContext,"Suggestion closed.",Toast.LENGTH_SHORT).show()
         }
 
 
-        // Set a click listener for root layout
-        root.root_layout.setOnClickListener{
-            val text = root.autocompleteTextView.text
-            Toast.makeText(FlightApplication.appContext,"Inputted : $text",Toast.LENGTH_SHORT).show()
-        }
-
-
         // Set a focus change listener for auto complete text view
-        root.autocompleteTextView.onFocusChangeListener = View.OnFocusChangeListener{
+        root.text_home.onFocusChangeListener = View.OnFocusChangeListener{
                 view, b ->
             if(b){
                 // Display the suggestion dropdown on focus
-                root.autocompleteTextView.showDropDown()
+                root.text_home.showDropDown()
             }
         }
 
+        switchD.setOnCheckedChangeListener { buttonView, isChecked ->
+
+            if(switchD.isChecked){
+                switchD.text = "Arrivee"
+                root.textViewD.isEnabled = false
+                root.dateD.isEnabled = false
+
+                root.textViewA.isEnabled = true
+                root.dateA.isEnabled = true
+
+            }else {
+                switchD.text = "Depart"
+                root.textViewD.isEnabled = true
+                root.dateD.isEnabled = true
+
+                root.textViewA.isEnabled = false
+                root.dateA.isEnabled = false
+            }
+        }
+
+        fromDate.setOnClickListener(View.OnClickListener { view ->
+            val currentCalendar = Calendar.getInstance()
+            val datePickerDialog = DatePickerDialog(
+                view.context,
+                fromDateListener,
+                currentCalendar.get(Calendar.YEAR),
+                currentCalendar.get(Calendar.MONTH),
+                currentCalendar.get(Calendar.DAY_OF_MONTH)
+            )
+            val datePicker = datePickerDialog.datePicker
+            currentCalendar.add(Calendar.DAY_OF_MONTH, -1)
+            datePicker.maxDate = currentCalendar.timeInMillis
+            datePickerDialog.show()
+        })
+
+        toDate.setOnClickListener(View.OnClickListener { view ->
+            val currentCalendar = Calendar.getInstance()
+            val datePickerDialog = DatePickerDialog(
+                view.context, toDateListener, currentCalendar.get(Calendar.YEAR),
+                currentCalendar.get(Calendar.MONTH), currentCalendar.get(Calendar.DAY_OF_MONTH)
+            )
+            val datePicker = datePickerDialog.datePicker
+            currentCalendar.add(Calendar.DAY_OF_MONTH, -1)
+            datePicker.maxDate = currentCalendar.timeInMillis
+            datePickerDialog.show()
+        })
+
+        root.buttonGO.setOnClickListener(View.OnClickListener {
+            val timePicked: String
+
+            if(switchD.isChecked == true) {
+                timePicked = toDate.text.toString()
+            }else{
+                timePicked = fromDate.text.toString()
+            }
+
+            Toast.makeText(FlightApplication.appContext, text_home.text.toString() + ' ' + timePicked, Toast.LENGTH_SHORT).show()
+        })
+
         return root
     }
+
+    fun updateDateLabel(dateEditText: EditText, calendar: Calendar) {
+        dateEditText.setText(
+            calendar.get(Calendar.DAY_OF_MONTH).toString() + "/" + calendar.get(
+                Calendar.MONTH
+            ) + "/" + calendar.get(Calendar.YEAR)
+        )
+    }
+
+    val fromDateListener =
+        DatePickerDialog.OnDateSetListener { datePicker, year, month, dayOfMonth ->
+            val calendar = Calendar.getInstance()
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, month)
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateDateLabel(fromDate, calendar)
+        }
+
+    val toDateListener =
+        DatePickerDialog.OnDateSetListener { datePicker, year, month, dayOfMonth ->
+            val calendar = Calendar.getInstance()
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, month)
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateDateLabel(toDate, calendar)
+        }
 }
+
+
+
+
